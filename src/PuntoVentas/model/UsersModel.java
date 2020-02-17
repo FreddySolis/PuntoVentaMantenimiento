f7085aea2d4750f2469d270591f3dc53faf65869
BraulioRuiz <43392482+BraulioRuiz@users.noreply.github.com>
2020-02-16 21:17:46 -0600

    login

--- src/PuntoVentas/model/UsersModel.java
+++ src/PuntoVentas/model/UsersModel.java
@@ -5,10 +5,53 @@
  */
 package PuntoVentas.model;
 
+import PuntoVentas.controller.ConnectorMySQL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Hinge
  */
 public class UsersModel {
     
+    public static String find_user(ConnectorMySQL cn,String usuario){
+        String user = null;
+        String usuarioSQL = "select nombre_usuario,admin from usuarios where nombre_usuario = '"+usuario+"'";
+        try {                          
+            Statement stt = cn.getConnection().createStatement();
+            ResultSet rs = stt.executeQuery(usuarioSQL);                
+            while(rs.next()){
+                user = rs.getString("nombre_usuario");
+                user = user +";"+rs.getString("admin");
+            }                
+               
+            return user;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return user = null;
+        }
+    }
+    
+    public static String get_password(ConnectorMySQL cn, String usuario){
+        String password = null;
+        String passwordSQL = "select password from usuarios where nombre_usuario = '"+usuario+"'";
+
+        try {                          
+            Statement stt = cn.getConnection().createStatement();
+            ResultSet rs = stt.executeQuery(passwordSQL);                
+            while(rs.next()){
+                password = rs.getString("password");
+            }                
+               
+            return password;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return password = null;
+        }
+        
+    }
 }
